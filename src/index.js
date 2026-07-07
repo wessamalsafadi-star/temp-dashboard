4e40f9fe73b0933a50c3f7730ed23fc9052ca76c
wessamalsafadi-star <wessam.alsafadi@bhomes.com>
2026-07-07 06:25:15 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import LeadDashboard from './LeadDashboard';
 import './index.css';
 
+const Page = window.location.pathname.startsWith('/leads') ? LeadDashboard : App;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<React.StrictMode><App /></React.StrictMode>);
+root.render(<React.StrictMode><Page /></React.StrictMode>);
